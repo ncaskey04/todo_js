ca3252594b4cd2949d3d3a0450774cee3c74c0c6
ncaskey04 <ncaskey04@gmail.com>
2014-07-15 21:43:55 -0700

    finished, it works

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 var form = document.getElementById("todo_form");
 var list = document.getElementById("todo_list");
 var input = document.getElementById('todo_input');
-var remove = document.getElementByTagName('button');
+var remove = document.getElementsByTagName('button');
+
 
 form.onsubmit = function(event) {
 	event.preventDefault();
 
-	var uList = document.createElement('ul');
+	var uList = document.createElement('li');
 	var newTextNode = document.createTextNode(input.value);
 	var rmButton = document.createElement('button');
+
+	input.value = "";
+
 
 	var addItem = function(event) {
 		list.innerHTML += "<ul>" + input.value + "</ul>"
 	}
 
+
+
 		rmButton.innerHTML = 'X'
 		list.appendChild(uList)
-		list.appendChild(newTextNode)
-		list.appendChild(rmButton)
+		uList.appendChild(newTextNode)
+		uList.appendChild(rmButton)
 
 	rmButton.onclick = function(event){
-		 rmButton.remove()
-		 newtextNode.remove()
+		 uList.remove();
 	}
 }
